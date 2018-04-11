25ddb002d58423f2d2f1a6b588e99eb09c3527e0
Garrett <garrett.voss4@gmail.com>
2018-04-11 11:12:36 -0500

    Changing info on will appear and will disappear

--- Ch.10/FirstViewController.swift
+++ Ch.10/FirstViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad() { // initial display of the scene only. Won't re-execute unless the view is unloaded from memory
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) { // gets called when the scene Will Reappear to the user
+        super.viewWillAppear(animated)
+        lblFirstViewLabel.text = "\(aNumber)"
+        aNumber = aNumber + 2
+        // Set up listeners here
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // do any clean up here that you want to do before the scenee is not showed to the user
+        // get rid of listeners here
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBOutlet weak var lblFirstViewLabel: UILabel!
+    var aNumber:Int = 1
+    
+    
 
 }
 
